316a5086861d08904dee5c797b2bcbe679439cb6
Florian Widmer <fwi@bsiag.com>
2019-08-01 16:42:39 +0200

    [FIX] include priorities in headline regex

--- src/regexes.js
+++ src/regexes.js
@@ -3,7 +3,7 @@ export const DATE_REGEX = /\d{4}-\d{1,2}-\d{1,2}/;
 export const TIMESTAMP_REGEX = /([<\[][^\]>]*[\]>])/;
 export const SCHEDULED_REGEX = /SCHEDULED: ([<\[][^\]>]*[\]>])/;
 export const DEADLINE_REGEX = /DEADLINE: ([<\[][^\]>]*[\]>])/;
-export const HEADLINE_REGEX = /^\*+\s([A-Z]*)\s*(.*)(:[^:]+:)*$/;
+export const HEADLINE_REGEX = /^\*+\s([A-Z]*)\s*(\[\#[ABC]\])?\s*(.*)(:[^:]+:)*$/;
 
 /**
  * Regular expression to find keyword in a headline. It will be in group 1
